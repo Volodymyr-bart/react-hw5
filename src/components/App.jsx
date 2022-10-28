bfcd6f6b7cf41642e6af161cde87aea0b058d340
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-10-28 13:01:48 +0300

    example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,22 @@ import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 
-const createAsyncComponent = path => lazy(() => import(path));
+// const createAsyncComponent = path => lazy(() => import(path));
 
-const About = createAsyncComponent('../pages/About');
-const Home = createAsyncComponent('../pages/Home');
-const ProductDetails = createAsyncComponent('../pages/ProductDetails');
-const Products = createAsyncComponent('../pages/Products');
-const Mission = createAsyncComponent('./Mission');
-const Team = createAsyncComponent('./Team');
-const Reviews = createAsyncComponent('./Reviews');
-
-// const Movies = lazy(() => import('../pages/Movies/Movies'));
-// const Home = lazy(() => import('../pages/Home/Home'));
-// const MovieDetails = lazy(() => import('../pages/MovieDetails/MovieDetails'));
-// const Cast = lazy(() => import('./Cast/Cast'));
-// const Reviews = lazy(() => import('./Reviews/Reviews'));
+// const About = createAsyncComponent('../pages/About');
+// const Home = createAsyncComponent('../pages/Home');
+// const ProductDetails = createAsyncComponent('../pages/ProductDetails');
+// const Products = createAsyncComponent('../pages/Products');
+// const Mission = createAsyncComponent('./Mission');
+// const Team = createAsyncComponent('./Team');
+// const Reviews = createAsyncComponent('./Reviews');
+const About = lazy(() => import('../pages/About'));
+const Home = lazy(() => import('../pages/Home'));
+const ProductDetails = lazy(() => import('../pages/ProductDetails'));
+const Products = lazy(() => import('../pages/Products'));
+const Mission = lazy(() => import('./Mission'));
+const Team = lazy(() => import('./Team'));
+const Reviews = lazy(() => import('./Reviews'));
 
 export const App = () => {
   return (
